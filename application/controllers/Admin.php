6507c63f584ca8f8bb54ecff3aabb22e6bb9d3d0
asnoveri <asnoveri15@gmail.com>
2020-07-15 21:53:50 +0700

    YIHU

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -17,19 +17,27 @@ class Admin extends CI_Controller
 
     public function index()
     {
+        $string = "2020-08-17";
+        $value = preg_replace("/[^0-9]/", "", $string);
+
+        $array =  json_decode(file_get_contents("https://raw.githubusercontent.com/guangrei/Json-Indonesia-holidays/master/calendar.json"), true);
+
+
+        //check tanggal merah berdasarkan libur nasional
+        if (isset($array[$value])) {
+            echo "tanggal merah " . $array[$value]["deskripsi"] . "<br>" . $value;
+        } elseif (date("D", strtotime($value)) === "Sun") {
+            echo "tanggal merah hari minggu" . "<br>" . $value;
+        } elseif (date("D", strtotime($value)) === "Sat") {
+            echo "tanggal merah hari Sabtu" . "<br>" . $value;
+        } else {
+            echo "bukan tanggal merah" . "<br>" . $value;
+        }
+        die();
         $judul = 'Dashboard';
         $halaman = 'admin/index';
         $data = "";
         $this->template->TemplateGen($judul, $halaman, $data);
-
-        //  $data = $this->Log_Model->get_log_activity();
-        // foreach ($data as $dt) {
-        //     $tanggal=$dt->tanggal;
-        //     $datestring = '%d-%m-%Y - %h:%i %a';
-        //     echo  $dt->id_user . " : ". mdate($datestring, $tanggal);  
-        //     echo "<br>";
-        // }
-        // die();
     }
 
     public function profil_admin()
@@ -41,42 +49,42 @@ class Admin extends CI_Controller
     }
 
 
-    public function cek()
-    {
-        $latitude1       = $this->input->post('latitudeUser', true);
-        $longitude1      = $this->input->post('longitudeUser', true);
+    // public function cek()
+    // {
+    //     $latitude1       = $this->input->post('latitudeUser', true);
+    //     $longitude1      = $this->input->post('longitudeUser', true);
 
-        // $latitude1          = 0.527241;
-        // $longitude1         = 101.434586;
-        $latitude2          = 0.525254;
-        $longitude2         = 101.434762;
+    //     // $latitude1          = 0.527241;
+    //     // $longitude1         = 101.434586;
+    //     $latitude2          = 0.525254;
+    //     $longitude2         = 101.434762;
 
-        $theta = $longitude1 - $longitude2;
-        $distance = (sin(deg2rad($latitude1)) * sin(deg2rad($latitude2)))  + (cos(deg2rad($latitude1)) * cos(deg2rad($latitude2)) * cos(deg2rad($theta)));
-        $distance = acos($distance);
-        $distance = rad2deg($distance);
-        $distance = $distance * 60 * 1.1515;
-        $distance = $distance * 1.609344;
-        $jarak_akhri = round($distance, 2);
-        echo json_encode($jarak_akhri);
-        // if ($jarak_akhri <= 0.3) {
-        //     // $this->index("add_absensi", $this->input->post());
-        //     $pesan = "Dapat Melakukan Absensi";
-        //     $this->session->set_flashdata('erorabsen', '<div class="alert alert-danger alert-dismissible">
-        //         <button type="button" class="close" data-dismiss="alert">&times;</button>'
-        //         . $pesan .
-        //         '</div>');
-        //     echo json_encode($pesan);
-        // } else {
-        //     $pesan = "Tidak Dapat Melakukan Pengambilan Absen Masuk, Karena Anda Tidak Berada Di wilayah Kantor";
-        //     $this->session->set_flashdata('erorabsen', '<div class="alert alert-danger alert-dismissible">
-        //         <button type="button" class="close" data-dismiss="alert">&times;</button>'
-        //         . $pesan .
-        //         '</div>');
-        //     echo json_encode($pesan);
-        // }
-        die();
-    }
+    //     $theta = $longitude1 - $longitude2;
+    //     $distance = (sin(deg2rad($latitude1)) * sin(deg2rad($latitude2)))  + (cos(deg2rad($latitude1)) * cos(deg2rad($latitude2)) * cos(deg2rad($theta)));
+    //     $distance = acos($distance);
+    //     $distance = rad2deg($distance);
+    //     $distance = $distance * 60 * 1.1515;
+    //     $distance = $distance * 1.609344;
+    //     $jarak_akhri = round($distance, 2);
+    //     echo json_encode($jarak_akhri);
+    //     // if ($jarak_akhri <= 0.3) {
+    //     //     // $this->index("add_absensi", $this->input->post());
+    //     //     $pesan = "Dapat Melakukan Absensi";
+    //     //     $this->session->set_flashdata('erorabsen', '<div class="alert alert-danger alert-dismissible">
+    //     //         <button type="button" class="close" data-dismiss="alert">&times;</button>'
+    //     //         . $pesan .
+    //     //         '</div>');
+    //     //     echo json_encode($pesan);
+    //     // } else {
+    //     //     $pesan = "Tidak Dapat Melakukan Pengambilan Absen Masuk, Karena Anda Tidak Berada Di wilayah Kantor";
+    //     //     $this->session->set_flashdata('erorabsen', '<div class="alert alert-danger alert-dismissible">
+    //     //         <button type="button" class="close" data-dismiss="alert">&times;</button>'
+    //     //         . $pesan .
+    //     //         '</div>');
+    //     //     echo json_encode($pesan);
+    //     // }
+    //     die();
+    // }
 
     public function listlog()
     {
